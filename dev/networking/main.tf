e736c6acd86932226508646ca6283aa670a0bcf2
Jaspreet Singh Marwah <jmarwah4@myseneca.ca>
2022-12-13 05:22:13 +0000

    Added more resources to dev/networking
    Added elastic IP. Added NAT gateway for private subnets to communicate through. Added route tables for private and public subnets. Added route table assosciations for the route tables.

--- dev/networking/main.tf
+++ dev/networking/main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
   tags = merge(
     var.default_tags, {
-      Name = "${local.name_prefix}-VPC"
+      Name = "${name_prefix}-VPC"
     }
   )
 }
@@ -63,4 +63,71 @@ resource "aws_internet_gateway" "igw" {
       "Name" = "${local.name_prefix}-IGW"
     }
   )
+}
+
+# Elastic IP
+resource "aws_eip" "eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-EIP"
+    }
+  )
+}
+
+# Nat Gateway for private subnets
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+  depends_on    = [aws_internet_gateway.igw]
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Nat-GW"
+    }
+  )
+}
+
+# Route table for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Private-Route-Table"
+    }
+  )
+}
+
+# Route table for public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Public-Route-Table"
+    }
+  )
+}
+
+# Assosciating public subnets with the public route table
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(aws_subnet.public_subnet[*].id)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+}
+
+# Assosciating private subnets with the private route table
+resource "aws_route_table_association" "private_route_table_association" {
+  count          = length(aws_subnet.private_subnet[*].id)
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
 }
