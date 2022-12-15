975279fbdcaffe7e99600d360822a9397ad0f225
neilsuryanarayanan <cristianoneil39@gmail.com>
2022-12-15 21:07:47 +0000

    adding changes to staging

--- staging/networking/config.tf
+++ staging/networking/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "staging-S3"      
+    bucket = "staging-s3"      
     key    = "network/terraform.tfstate" 
     region = "us-east-1"
   }
--- staging/networking/variables.tf
+++ staging/networking/variables.tf
@@ -19,3 +19,29 @@ variable "env" {
   description = "Staging Environment"
 }
 
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "AWS region in which our architecture is being deployed"
+}
+
+variable "vpc_cidr" {
+  default     = "10.200.0.0/16"
+  type        = string
+  description = "CIDR range of VPC for the dev environment"
+}
+
+# Public subnets in VPC
+variable "public_subnet_cidrs" {
+  default     = ["10.200.0.0/24", "10.200.1.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
+
+# Private subnets in VPC
+variable "private_subnet_cidrs" {
+  default     = ["10.200.2.0/24", "10.200.3.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs"
+}
+
