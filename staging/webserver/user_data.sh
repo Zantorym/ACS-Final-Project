ec9ee541d20e490fd32d80582cf09fb6f8b80169
neilsuryanarayanan <cristianoneil39@gmail.com>
2022-12-17 18:36:00 +0000

    web server deployment in staging

---
+++ staging/webserver/user_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h1>Welcome to Group 13 ACS Project! My private IP is $myip <br> Group Members are Harsh Shah, Jaishree Jaishankar, Jaspreet Marwah, Neil Suryanarayanan</h1>" >  /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
