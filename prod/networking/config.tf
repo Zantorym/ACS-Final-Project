00efc58776857c6c105d2db656745063b5ee922a
Harsh Shah <hashah7@myseneca.ca>
2022-12-22 08:35:34 +0000

    Minor changes in webserver config in PROD env

--- prod/networking/config.tf
+++ prod/networking/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "prod-s3-acsgroup13"
-    key    = "prod-network/terraform.tfstate"
+    key    = "prod-networking/terraform.tfstate"
     region = "us-east-1"
   }
 }
