ae6fedfc39ce36c9be1ca9dd400c1e46487e997c
Harsh Shah <hashah7@myseneca.ca>
2022-12-22 00:16:03 +0000

    Modified main and var files in PROD environment

--- prod/networking/variables.tf
+++ prod/networking/variables.tf
@@ -19,14 +19,20 @@ variable "env" {
   description = "Production Environment"
 }
 
+variable "vpc_cidr" {
+  default = "10.250.0.0/16"
+  type = string
+  description = "CIDR range of VPC for the PROD environment"
+}
+
 variable "public_subnet_cidrs" {
-  default     = ["10.200.0.0/24", "10.200.1.0/24", "10.200.2.0/24"]
+  default     = ["10.250.0.0/24", "10.250.1.0/24", "10.250.2.0/24"]
   type        = list(string)
   description = "Public subnet CIDRs"
 }
 
 variable "private_subnet_cidrs" {
-  default     = ["10.200.3.0/24", "10.200.4.0/24", "10.200.5.0/24"]
+  default     = ["10.250.3.0/24", "10.250.4.0/24", "10.250.5.0/24"]
   type        = list(string)
   description = "Private subnet CIDRs"
 }
