66158b4ec086bb1fa8578e10147df9c8ba2bf4bf
Harsh Shah <hashah7@myseneca.ca>
2022-12-23 08:55:38 +0000

    Networking modified as per modules

--- environments/prod/networking/config.tf
+++ environments/prod/networking/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "prod-s3-acsgroup13"
-    key    = "prod-networking/terraform.tfstate"
+    key    = "prod-network/terraform.tfstate"
     region = "us-east-1"
   }
 }
